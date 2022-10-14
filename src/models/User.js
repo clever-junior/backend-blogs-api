7580b7ed3ed2e201588ffc94d161dc04319b60b0
clever-junior <tk_clever.junior@outlook.com>
2022-10-13 21:57:30 -0400

    feat: adiciona requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.DATE,
+    image: DataTypes.DATE
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'users',
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'posts' });
+  };
+
+  return User;
+};
